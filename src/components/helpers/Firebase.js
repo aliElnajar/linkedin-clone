db72bbf302f057d950a5bcda2065a2ba33dd3286
aliElnajar <116386046+aliElnajar@users.noreply.github.com>
2023-04-13 20:12:05 +0200

    another update

--- src/components/helpers/Firebase.js
+++ src/components/helpers/Firebase.js
@@ -2,7 +2,7 @@ import firebase from "firebase/compat/app";
 import "firebase/compat/firestore";
 import { collection } from "firebase/firestore";
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
+  apiKey: "AIzaSyCspv28E679GKZmZZqkMIP5Sbm_4su5Zyw",
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
